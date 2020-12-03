5deb5bd005ef527b8971d74331c6b35ae2c7338d
jeonyeohun <21500630@handong.edu>
2020-12-03 12:38:49 +0900

    Update P2.rkt

--- HW4/P2.rkt
+++ HW4/P2.rkt
@@ -1,4 +1,10 @@
 #lang plai
+
+; Solved by myself: N
+; Time Taken: 20 hrs
+
+;[contract]
+;[purpose]
 (define (run sexp ds st)
   (interp (parse sexp) ds st))
 
@@ -26,38 +32,46 @@
   [boxV (address integer?)]
   [exprV (expr BFAE?) (ds DefrdSub?) (st Store?) (value (box/c (or/c false BFAE-Value?)))])
 
+;[contract]
+;[purpose]
 (define (strict v)
-  ;(displayln (list 'strict-val: v))
   (type-case Value*Store v
     [v*s (v1 st1)
          (type-case BFAE-Value v1
-           [exprV (expr ds st1 v1-box)
+           [exprV (expr ds st2 v1-box)
                   (if (not (unbox v1-box))
                       (local [(define v2 (strict (interp expr ds st1)))]
-                        ;(displayln (list 'innervs: v2))
                         (type-case Value*Store v2
                           [v*s (v2-val v2-sto)
                                (begin (set-box! v1-box v2-val)
                                       v2)]))
-                      (v*s (unbox v1-box)))]
+                      (v*s (unbox v1-box) st1))]
            [else v])]))
 
 (define-type Value*Store
   [v*s (value BFAE-Value?) (store Store?)])
 
+;[contract]
+;[purpose]
 (define (malloc st)
   (+ 1 (max-address st)))
 
+;[contract]
+;[purpose]
 (define (max-address st)
   (type-case Store st
     [mtSto () 0]
     [aSto (n v st)
           (max n (max-address st))]))
 
+;[contract]
+;[purpose]
 (define-type DefrdSub
   [mtSub]
   [aSub (name symbol?) (address integer?) (ds DefrdSub?)])
 
+;[contract]
+;[purpose]
 (define (lookup name ds)
   (type-case DefrdSub ds
     [mtSub() (error 'lookup "free id")]
@@ -65,6 +79,8 @@
                             adr
                             (lookup name saved))]))
 
+;[contract]
+;[purpose]
 (define (store-lookup address sto)
   (type-case Store sto
     [mtSto() (error 'store-lookup "No value")]
@@ -73,9 +89,8 @@
              value
              (store-lookup address rest-store))]))
 
-
-
-; Parser
+;[contract]
+;[purpose]
 (define (parse sexp)
   (match sexp
     [(? number?)                    (num sexp)]
@@ -87,19 +102,29 @@
     [(list 'openbox b)              (openbox (parse b))]
     [(list 'newbox v)               (newbox (parse v))]
     [(? symbol?)                    (id sexp)]
-    [(list 'fun (list p) b)         (fun p (parse b))] ; we need to parse function body because it also contains sexp. we need to convert it the FWAE format. i.e. {fun {x} {+ x 1}} -> {+ x 1} is a sexp. it should be converted to (add (id x) (number 1))
+    [(list 'fun (list p) b)         (fun p (parse b))]
     [(list f a)                     (app (parse f) (parse a))]
     [else                           (error 'parse sexp)]
   )
 )
 
+;[contract]
+;[purpose]
 (define (num-op op x y)
     (numV (op (numV-n x) (numV-n y))))
+
+;[contract]
+;[purpose]
 (define (num+ x y) (num-op + x y))
+
+;[contract]
+;[purpose]
 (define (num- x y) (num-op - x y))
 
+;[contract]
+;[purpose]
 (define (interp expr ds st)
-  (displayln (list 'expr: expr 'ds: ds 'st: st))
+  ;(displayln (list 'expr: expr 'ds: ds 'st: st))
   (type-case BFAE expr
     [num (n) (v*s (numV n) st)]
     [id (s) (v*s (store-lookup (lookup s ds) st) st)]
@@ -109,7 +134,7 @@
     [app (f a) (type-case Value*Store (strict (interp f ds st))
                  [v*s (f-value f-store)
                       (type-case BFAE a
-                        [newbox (val) (type-case Value*Store (interp a ds f-store)
+                        [newbox (val) (type-case Value*Store (strict (interp a ds f-store))
                                      [v*s (a-value a-store)
                                           (local ([define new-address (malloc a-store)])
                                             (interp (closureV-body f-value)
@@ -120,27 +145,25 @@
                                                           a-value
                                                           a-store)))])]
                         [else (local([define new-address (malloc f-store)])
-                        (define a-val (exprV a ds st (box #f)))
-                        (interp (closureV-body f-value)
-                                (aSub (closureV-param f-value)
-                                      new-address
-                                      (closureV-ds f-value))
-                                (aSto new-address
-                                      a-val
-                                      f-store)))]
+                                (define a-val (exprV a ds st (box #f)))
+                                (interp (closureV-body f-value)
+                                        (aSub (closureV-param f-value)
+                                              new-address
+                                              (closureV-ds f-value))
+                                        (aSto new-address
+                                              a-val
+                                              f-store)))]
                           )])]
     [seqn (a b) (interp-two a b ds st (lambda (v1 v2 st1)(v*s v2 st1)))]
-    [newbox (val) (type-case Value*Store (strict (interp val ds st))
-                    [v*s (vl st1)
-                         (local[(define a (malloc st1))
-                                (define l (exprV (num (numV-n vl)) ds st1 (box #f)))]
+    [newbox (val) (local[(define a (malloc st))
+                         (define l (exprV val ds st (box #f)))]
                            (v*s(boxV a)
-                               (aSto a l st1)))])]
+                               (aSto a l st)))]
     [openbox (bx-expr) (type-case Value*Store (interp bx-expr ds st)
                          [v*s (bx-val st1)
-                              (v*s(store-lookup (boxV-address bx-val)
+                              (strict(v*s(store-lookup (boxV-address bx-val)
                                                 st1)
-                                  st1)])]
+                                  st1))])]
     [setbox (bx-expr val-expr)
             (strict (interp-two bx-expr val-expr ds st
                         (lambda (bx-val val st1)
@@ -151,11 +174,11 @@
     )
   ) 
 
+;[contract]
+;[purpose]
 (define (interp-two expr1 expr2 ds st handle)
   (type-case Value*Store (strict (interp expr1 ds st))
     [v*s (val1 st2)
          (type-case Value*Store (strict (interp expr2 ds st2))
            [v*s (val2 st3)
                 (handle val1 val2 st3)])]))
-
-
